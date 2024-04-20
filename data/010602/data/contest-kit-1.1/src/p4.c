1c30b7b48d5e3ce298529f521d4fb8323c151664
prsabahrami <pbahrami@uwaterloo.ca>
2024-04-20 16:38:06 -0400

    Rename folder: data to contests

--- data/010602/data/contest-kit-1.1/src/p4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-main(){
-   int n,rem,digs;
-   while (1 == scanf("%d",&n)) {
-      for (rem=digs=1;rem;digs++) rem = (rem*10+1) % n;
-      printf("%d\n",digs);
-   }
-}
--- data/020928/data/Bgen.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int i,j,k;
-
-main(){
-   for (i=0;i<100;i++) {
-      printf("%d %d\n",random(),random());
-   }
-}
